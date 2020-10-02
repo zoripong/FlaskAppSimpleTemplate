8bf7e69f15de0248e91fab24d71f44d37c520503
yool <yool@spoqa.com>
2020-10-02 21:54:32 +0900

    Add install shell script

---
+++ scripts/install.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -e
+
+if [[ ! -f .git/hooks/pre-commit ]]; then
+  cwd="$( cd "$(dirname "$0")/.." ; pwd -P )"
+  git config core.hooksPath "$cwd/hooks"
+fi
+
+if ! hash poetry 2>/dev/null; then
+  curl -sSL https://raw.githubusercontent.com/sdispater/poetry/master/get-poetry.py | python3
+fi
+
+if [[ "$VIRTUAL_ENV" = "" ]]; then
+  echo 'You seem not in any virtual environment.  Stopped.' >> /dev/stderr
+  exit 1
+fi
+
+poetry install
