44ac014e2aad1241a6858405bfd2c32624e9126d
yool <yool@spoqa.com>
2020-10-04 23:50:00 +0900

    fix

--- scripts/install.sh
+++ scripts/install.sh
@@ -8,6 +8,7 @@ fi
 
 if ! hash poetry 2>/dev/null; then
   curl -sSL https://raw.githubusercontent.com/sdispater/poetry/master/get-poetry.py | python3
+  source $HOME/.poetry/env
 fi
 
 if [[ "$VIRTUAL_ENV" = "" ]]; then
